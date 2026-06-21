655cb9829eed8b8424b02d5de100900959cb0998
Sanjeev Thapa <116951750+sanjeevlcc@users.noreply.github.com>
2026-06-21 18:24:43 +0545

    Update db.php

--- CRUD_in_crud/1.crud_ubuntu_flat/db.php
+++ CRUD_in_crud/1.crud_ubuntu_flat/db.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
-$dbname = "crud_app";
-$username = "crud_user";
+$dbname = "restaurant_crud";
+$username = "restaurant_user";
 $password = "apple123";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
